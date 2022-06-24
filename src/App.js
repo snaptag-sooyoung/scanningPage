0a8951b04bce515e1c2c325d4a0c16c74643949d
sooyoung <sooyoung159@snaptag.co.kr>
2022-06-24 12:08:11 +0900

    타이머 표시 후 이동

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [startTime, setStartTime] = useState();
+  const [leftTime, setLeftTime] = useState(3);
+
+  useEffect(() => {
+    if (leftTime === 0) {
+      window.location.replace("http://www.naver.com");
+    } else {
+      setTimeout(() => {
+        setLeftTime(leftTime - 1);
+      }, 1000);
+    }
+  }, [leftTime]);
+
+  return <div className="App">{leftTime}초 후 페이지 이동</div>;
 }
 
 export default App;
